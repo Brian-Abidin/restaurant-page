c3ae66057abf7bdba2022d233a3363909a3a63e8
Brian Abidin <Brian.abidin1@gmail.com>
2023-03-21 18:22:52 -0400

    added variables for html elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("restaurant page");
+console.log("Hello World");
+
+const content = document.getElementById("content");
+const wrapper = document.createElement("div");
+const header = document.createElement("div");
+const rightlinks = document.createElement("ul");
+const socials = document.createElement("div");
+const front_image = document.createElement("div");
+const description = document.createElement("div");
+const desc_text = document.createElement("div");
+const desc_title = document.createElement("div");
+const desc_image = document.createElement("div");
+const footer = document.createElement("footer");
+const image1 = document.createElement("img");
+const image2 = document.createElement("img");
+
+content.appendChild(wrapper);
+
