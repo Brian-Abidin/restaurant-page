774c9ef31465a6e7cb7546b056ab4a6242afe801
Brian Abidin <Brian.abidin1@gmail.com>
2023-03-24 18:49:28 -0400

    added onclick event when menu is clicked

--- src/menu.js
+++ src/menu.js
@@ -2,3 +2,14 @@
 on click loads the HTML elements
 create HTML elements. removes the divs that are showing
 and adds the divs for the Menu tab */
+
+menu.addEventListener("click", ()=> {
+    removeStyle(home);
+    removeStyle(contact);
+    addStyle(menu);
+
+
+    midsection.replaceChildren();
+
+    midsection.textContent = "HELLO THERE"
+});
