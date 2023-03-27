26635e8658ec5777a62449501570b63725c61177
Brian Abidin <Brian.abidin1@gmail.com>
2023-03-27 19:25:47 -0400

    added new menu item

--- src/menu.js
+++ src/menu.js
@@ -5,11 +5,13 @@ and adds the divs for the Menu tab */
 
 import {home, menu, contact, midsection, leftItem1, 
         middleItem1, rightItem1, menuItem1, menuItem2,
-        menuItem3, price2, leftItem2, middleItem2, rightItem2, itemName2} from  "./domElements"
+        menuItem3, itemPrice2, leftItem2, middleItem2, rightItem2, 
+        itemName2, leftItem3, itemName3, itemPrice3, rightItem3,
+        middleItem3} from  "./domElements"
 import removeStyle from "./functions/removeStyle"
 import addStyle from "./functions/addStyle"
 import spaghetti from "./images/spaghetti.jpg"
-
+import lobster from "./images/lobster.png"
 
 export default function addMenu(){
     removeStyle(home);
@@ -25,7 +27,9 @@ export default function addMenu(){
     midsection.style.display = "grid";
     midsection.style.marginLeft = "200px";
     midsection.style.marginRight = "200px";
-    midsection.style.gridTemplateRows = "200px, repeat(2, 1fr)";
+    midsection.style.gridTemplateRows = "200px, repeat(2, )";
+    midsection.style.gap = "10px"
+    midsection.style.minWidth = "540px"
 
     menuItem1.appendChild(leftItem1);
     menuItem1.appendChild(middleItem1);
@@ -36,17 +40,26 @@ export default function addMenu(){
     menuItem2.appendChild(rightItem2);
 
     leftItem2.appendChild(itemName2);
-    leftItem2.appendChild(price2);
-
-    // menuItem1.textContent = "Menu item 1 and price"
-    // menuItem2.textContent = "Description of item"
-    // menuItem3.textContent = "image"
+    leftItem2.appendChild(itemPrice2);
 
     middleItem1.textContent = "Dinner";
+
     itemName2.textContent = "Spaghetti and Meatballs";
-    price2.textContent = "$17.99";
+    itemPrice2.textContent = "$17.99";
     middleItem2.textContent = "A large bowl of delicious in house made spaghetti and beef meatballs with freshly made tomato and basil based sauce. Topped with italian parmesean."
     rightItem2.src = spaghetti;
+
+    menuItem3.appendChild(leftItem3);
+    menuItem3.appendChild(middleItem3);
+    menuItem3.appendChild(rightItem3);
+
+    leftItem3.appendChild(itemName3);
+    leftItem3.appendChild(itemPrice3);
+
+    itemName3.textContent = "Red Lobster with Clam Chowder";
+    itemPrice3.textContent = "$24.99";
+    middleItem3.textContent = "A locally caught cooked Red Lobster served with a side of Clam Chowder and savory butter sauce"
+    rightItem3.src = lobster;
 
 
     // midsection.textContent = "HELLO THERE"
