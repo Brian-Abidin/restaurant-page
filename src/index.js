5fadaec1d8524c03c1b89e06567dbbeeba5235ae
Brian Abidin <Brian.abidin1@gmail.com>
2023-03-27 13:21:15 -0400

    added click event listener when home, menu, or contact is clicked

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import "./style.css";
 import load from "./functions/onLoad";
+import addMenu from "./menu"
+import addContact from "./contact"
+import {home, menu, contact} from "./constants"
+import addHome from "./home";
 
-// window.addEventListener("load", () => {
-//     load();
-// });
+
 load();
-// addEventListener("click", (e) => {
 
-// });
+home.addEventListener("click", (e) => {
+    addHome();
+});
+
+menu.addEventListener("click", (e) =>{
+    addMenu();
+});
+
+contact.addEventListener("click", (e) =>{
+    addContact();
+});
