c2dee06392aa91a791151cecf661de6711c26a1b
Brian Abidin <Brian.abidin1@gmail.com>
2023-03-27 13:04:40 -0400

    imported constants correctly

--- src/functions/onLoad.js
+++ src/functions/onLoad.js
@@ -1,54 +1,56 @@
-import * as dom from "../constants"
+import {content, wrapper, header, title, rightlinks, home, menu, contact, 
+        emptyheader, midsection, frontImage, image1, image2, description, 
+        descText, descText2, descTitle, descImage, footer} from  "../constants"
 import surfandturf from "../images/surfandturf.jpeg"
 import seafood from "../images/seafood.jpg"
 
 export default function load(){
-    dom.content.appendChild(dom.wrapper);
+    content.appendChild(wrapper);
 
     /* wrapper */
-    dom.wrapper.appendChild(dom.header);
-    dom.wrapper.appendChild(dom.midsection);
-    dom.wrapper.appendChild(dom.footer);
+    wrapper.appendChild(header);
+    wrapper.appendChild(midsection);
+    wrapper.appendChild(footer);
 
     /* header */
-    dom.header.appendChild(dom.title);
-    dom.header.appendChild(dom.rightlinks);
-    dom.header.appendChild(dom.emptyheader);
+    header.appendChild(title);
+    header.appendChild(rightlinks);
+    header.appendChild(emptyheader);
 
-    dom.title.textContent = "Elegant Food";
+    title.textContent = "Elegant Food";
 
-    dom.rightlinks.appendChild(dom.home);
-    dom.rightlinks.appendChild(dom.menu);
-    dom.rightlinks.appendChild(dom.contact);
+    rightlinks.appendChild(home);
+    rightlinks.appendChild(menu);
+    rightlinks.appendChild(contact);
 
-    dom.home.textContent = "Home";
-    dom.menu.textContent = "Menu";
-    dom.contact.textContent = "Contact";
+    home.textContent = "Home";
+    menu.textContent = "Menu";
+    contact.textContent = "Contact";
 
     /* midsection */
-    dom.midsection.appendChild(dom.frontImage);
-    dom.midsection.appendChild(dom.description);
+    midsection.appendChild(frontImage);
+    midsection.appendChild(description);
 
-    dom.frontImage.appendChild(dom.image1);
+    frontImage.appendChild(image1);
     // image1.src = "./images/surfandturf.jpeg";
-    dom.image1.src = surfandturf;
-    dom.image1.alt = "black plate on a table that includes cooked lobster cut in half on the left, a baked potato cut up in the middle, and a steak cut in slices on the right"
+    image1.src = surfandturf;
+    image1.alt = "black plate on a table that includes cooked lobster cut in half on the left, a baked potato cut up in the middle, and a steak cut in slices on the right"
 
     /* description */
-    dom.description.appendChild(dom.descText);
-    dom.description.appendChild(dom.descImage);
+    description.appendChild(descText);
+    description.appendChild(descImage);
 
-    dom.descText.appendChild(dom.descTitle);
-    dom.descText.appendChild(dom.descText2)
+    descText.appendChild(descTitle);
+    descText.appendChild(descText2)
 
-    dom.descTitle.textContent = "Delicious Food Awaits!";
-    dom.descText2.textContent = "Our food is made from fresh daily with the finest ingredients local to the area! Red Lobsters, Blue Oysters, Jumbo Shrimp, Fried Calamari, Filet Mingon, Ribeye, and more. From the surf to the turf and every delicious dish in between, this restuarant offers it all!"
+    descTitle.textContent = "Delicious Food Awaits!";
+    descText2.textContent = "Our food is made from fresh daily with the finest ingredients local to the area! Red Lobsters, Blue Oysters, Jumbo Shrimp, Fried Calamari, Filet Mingon, Ribeye, and more. From the surf to the turf and every delicious dish in between, this restuarant offers it all!"
    
-    dom.descImage.appendChild(dom.image2);
+    descImage.appendChild(image2);
     // image2.src = "./images/seafood.jpg"
-    dom.image2.src = seafood;
-    dom.image2.alt = "bowl of seafood including lobster, shrimp, small octopus, over a bed of lettuce."
+    image2.src = seafood;
+    image2.alt = "bowl of seafood including lobster, shrimp, small octopus, over a bed of lettuce."
 
     /* footer */
-    dom.footer.textContent = "The Odin Project 2023";
+    footer.textContent = "The Odin Project 2023";
 }
