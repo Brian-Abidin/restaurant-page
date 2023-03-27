b32d87e450bcc8722f984f2e62ff900cae84e504
Brian Abidin <Brian.abidin1@gmail.com>
2023-03-27 13:17:21 -0400

    imported constants and added addmenu function

--- src/menu.js
+++ src/menu.js
@@ -2,8 +2,12 @@
 on click loads the HTML elements
 create HTML elements. removes the divs that are showing
 and adds the divs for the Menu tab */
+import {home, menu, contact, midsection} from  "./constants"
+import removeStyle from "./functions/removeStyle"
+import addStyle from "./functions/addStyle"
 
-menu.addEventListener("click", ()=> {
+
+export default function addMenu(){
     removeStyle(home);
     removeStyle(contact);
     addStyle(menu);
@@ -12,4 +16,4 @@ menu.addEventListener("click", ()=> {
     midsection.replaceChildren();
 
     midsection.textContent = "HELLO THERE"
-});
+}
