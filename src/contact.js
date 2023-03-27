41592c0bb395532f44ed939e5f2295691a6a1619
Brian Abidin <Brian.abidin1@gmail.com>
2023-03-27 13:17:50 -0400

    imported functions and constants and export addConstant function

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,13 @@
-menu.addEventListener("click", ()=> {
+import {home, menu, contact, midsection} from  "./constants"
+import removeStyle from "./functions/removeStyle"
+import addStyle from "./functions/addStyle"
+
+export default function addContact(){
     removeStyle(home);
     removeStyle(contact);
     addStyle(menu);
 
-
     midsection.replaceChildren();
 
     midsection.textContent = "HELLO THERE"
-});
+} 
