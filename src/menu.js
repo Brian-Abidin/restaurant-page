d1e3d2e4f26c5e7fe033e50a9401ce9e79ffbd41
Brian Abidin <Brian.abidin1@gmail.com>
2023-03-27 13:50:25 -0400

    added dom elements for menu section

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,9 @@
 on click loads the HTML elements
 create HTML elements. removes the divs that are showing
 and adds the divs for the Menu tab */
-import {home, menu, contact, midsection} from  "./constants"
+import {home, menu, contact, midsection, leftMenu, 
+        middleMenu, rightMenu, menuItem1, menuItem2,
+        menuItem3} from  "./domElements"
 import removeStyle from "./functions/removeStyle"
 import addStyle from "./functions/addStyle"
 
@@ -12,8 +14,21 @@ export default function addMenu(){
     removeStyle(contact);
     addStyle(menu);
 
-
     midsection.replaceChildren();
 
-    midsection.textContent = "HELLO THERE"
+    midsection.appendChild(menuItem1);
+    midsection.appendChild(menuItem2);
+    midsection.appendChild(menuItem3);
+
+    midsection.style.display = "grid";
+    midsection.style.marginLeft = "100px";
+    midsection.style.marginRight = "100px";
+    midsection.style. gridTemplateRows = "repeat(3, 1fr)";
+
+
+    menuItem1.textContent = "Menu item 1 and price"
+    menuItem2.textContent = "Description of item"
+    menuItem3.textContent = "image"
+
+    // midsection.textContent = "HELLO THERE"
 }
